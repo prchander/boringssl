bb0998ae79f5d68f2b84f1732080924027d4668f
pi <pchandra@uccs.edu>
2021-05-16 15:30:03 +0000

    adding bssl expirements

---
+++ QUIC-project/Singleclient.py
@@ -0,0 +1,25 @@
+import os
+import socket
+
+def getIP():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 1))
+	return s.getsockname()[0]
+
+bssl_dir = os.path.expanduser('~/oqs/boringssl/build/tool/bssl')
+cert_dir = os.path.expanduser('~/oqs/boringssl/QUIC-project/rsa/key_CA.pem')
+
+print(f'Certificate Directory: {cert_dir}')
+n = 1
+
+client_ip = getIP()
+print(f'Client IP: {client_ip}')
+print(f'Number of samples: ', n)
+serverIP = input('Please enter the server IP: ')
+
+myCmd=f'{bssl_dir} client -connect 10.0.0.157:44330 -root-certs {cert_dir}'
+
+
+while n>0:
+    os.system(myCmd)
+    n -= 1
--- QUIC-project/server.py
+++ QUIC-project/server.py
@@ -1,8 +1,40 @@
-import os 
+import os
+import socket
 
-bssl_dir = os.path.expanduser('~/oqs/boringssl/build/tool/bssl')
-cert = os.path.expanduser('~/oqs/boringssl/new_certs/')
+print('Server has started.')
 
-#myCmd = f'{bssl_dir} server -cert {cert}/dilithium2/key_crt.pem -key {cert}/dilithium2/key_srv.pem -accept 44330'
-myCmd = f'{bssl_dir} server -cert {cert}rainbowVclassic/key_crt.pem -key {cert}rainbowVclassic/key_srv.pem -accept 44330'
-os.system(myCmd)
+def getIP():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 1))
+	return s.getsockname()[0]
+
+alg = 'Dilithium2'#input("Enter alg for server: ")
+lsquic_dir = os.path.expanduser('~/oqs/lsquic')
+ip = getIP()
+print(f'The server IP is {ip}')
+
+if alg == "Dilithium2":
+	myCmd =  f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/Dilithium2/key_crt.pem,{lsquic_dir}/certs/Dilithium2/key_srv.pem -s {ip}:4433 -g -j'
+	os.system(myCmd)
+
+if alg == "Dilithium3":
+	myCmd = f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/Dilithium3/key_crt.pem,{lsquic_dir}/certs/Dilithium3/key_srv.pem -s {ip}:4433'
+	os.system(myCmd)
+
+if alg == "Dilithium4":
+	myCmd = f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/Dilithium4/key_crt.pem,{lsquic_dir}/certs/Dilithium4/key_srv.pem -s {ip}:4433'
+	os.system(myCmd)
+	
+if alg =="Falcon512":
+	myCmd = f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/falcon512/key_crt.pem,{lsquic_dir}/certs/falcon512/key_srv.pem -s {ip}:4433'
+	os.system(myCmd)
+
+if alg == "Falcon1024":
+	myCmd = f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/falcon1024/key_crt.pem,{lsquic_dir}/certs/falcon1024/key_srv.pem -s {ip}:4433'
+	os.system(myCmd)
+
+if alg == "rsa":
+	myCmd = f'{lsquic_dir}/build/bin/./research_server -c www.example.com,{lsquic_dir}/certs/rsa/key_crt.pem,{lsquic_dir}/certs/rsa/key_srv.pem -s {ip}:4433'
+	os.system(myCmd)
+
+print('Success')
